0769272cdd7088a645a8738eaf99c9b09e38aa03
陳羿豐 Yi-Feng Chen <randomfrank8@gmail.com>
2022-11-01 00:18:35 +0800

    set lyric font

--- js/Text.js
+++ js/Text.js
@@ -1,7 +1,7 @@
 class TextLayout extends Layout {
     constructor(text) {
         super();
-        var {width, height} = getTextSize(text, 12+'px');
+        var {width, height} = getTextSize(text, 16+'px');
         this.text = text;
         this.width = width + 3;
         this.height = height;
@@ -11,7 +11,7 @@ class TextLayout extends Layout {
         var elt = document.createElementNS('http://www.w3.org/2000/svg', 'text');
         elt.setAttribute('x', this.x + 2);
         elt.setAttribute('y', this.y);
-        elt.style.fontSize = '12px';
+        elt.style.fontSize = '16px';
         elt.textContent = this.text;
         return elt;
     }
